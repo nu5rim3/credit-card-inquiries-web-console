1f6d9989e63dcb6bc42695ddec85328eefea088b
Nadeen Gamage <nadeengamage@gmail.com>
2021-02-08 17:38:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.disable('x-powered-by');
 app.use(function(req, res, next) {
   res.setHeader("content-security-policy", "upgrade-insecure-requests; frame-ancestors 'self' https://creditcard.lolcfinance.com");
   res.setHeader("strict-transport-security", "max-age=31536000");
-  // res.setHeader("Set-Cookie", "Domain=creditcard.lolcfinance.com");
+  res.setHeader("Referrer-Policy", "no-referrer");
   res.setHeader("X-Content-Type-Options", "nosniff");
   res.setHeader("x-frame-options", "SAMEORIGIN");
   res.setHeader("x-xss-protection", "1; mode=block");
