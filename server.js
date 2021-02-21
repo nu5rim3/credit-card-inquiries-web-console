d2ccf61061163d65eb6b63077043d383d276670b
Nadeen Gamage <nadeengamage@gmail.com>
2021-02-21 14:34:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ app.use('/robots.txt', function (req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/admin/profile', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
