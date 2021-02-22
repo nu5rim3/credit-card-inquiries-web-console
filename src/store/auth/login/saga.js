989df30acb915547d6dce8eb98612067df3a9a3a
Nadeen Gamage <nadeengamage@gmail.com>
2021-02-22 19:32:00 +0530

    Latest release

--- src/store/auth/login/saga.js
+++ src/store/auth/login/saga.js
@@ -19,7 +19,7 @@ export async function login(user, history) {
     let response = await new Promise(resolve => {
       var xhr = new XMLHttpRequest();
       xhr.open("POST", '/services/auth/realms/master/protocol/openid-connect/token', true);
-      xhr.setRequestHeader('Authorization', 'Basic YWRtaW4tY29uc29sZToyNTBiNWIzZi04NWUwLTQzNjYtYTYxNS04ZTAzZjBjOTdjMzc=')
+      xhr.setRequestHeader('Authorization', 'Basic YWRtaW4tY29uc29sZTplYzU2MTFlNC02MDlhLTRiNTItOTQzZS1hMDFlN2ZjYjBlNGI=')
       xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       xhr.onload = function (e) {
         resolve({ data: JSON.parse(xhr.response), status: xhr.status });
